527cb2af6386015e35918106b7541f8609381b96
Anton Shiryaev <med_phisiker@mail.ru>
2022-08-22 11:41:14 +1000

    Update my_telegram_bot_info.py

--- my_telegram_bot_info.py
+++ my_telegram_bot_info.py
@@ -1,4 +1,4 @@
-token = '5704996091:AAFXJh2GHUTUBw6AMZBH_TpBEA7NKxm5SZs'
-chat_id = '864329814'
-link2bot = 't.me/python_tqdm_bot'
-link2messages = 'https://api.telegram.org/bot5704996091:AAFXJh2GHUTUBw6AMZBH_TpBEA7NKxm5SZs/getUpdates'
+token = 'my_token'
+chat_id = 'my_chat_id'
+link2bot = 'my_telegram_bot_link'
+link2messages = 'https://api.telegram.org/bot{my_token}/getUpdates'
